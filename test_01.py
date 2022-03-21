7ecbe70d03508291af99e46f17512deae8d3323f
peixuan <806252233@qq.com>
2022-03-21 15:17:57 +0800

    Flask文件

--- test_01.py
+++ test_01.py
@@ -1,6 +1,21 @@
-import keyword
+# import keyword
 
-#输出当前版本所有关键字
-print(keyword.kwlist) 
+# #输出当前版本所有关键字
+# print(keyword.kwlist) 
 
-print('I\'m ok')
+# print('I\'m ok')
+#------------------------------------------
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hell_world():
+    return "hello world"
+
+@app.route('/get',methods=["get"])
+def get_():
+    return "get请求"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=7777)
